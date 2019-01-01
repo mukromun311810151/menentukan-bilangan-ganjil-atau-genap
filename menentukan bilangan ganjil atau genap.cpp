6863e56d9843818c34b19c7df0c4650470a44393
mukromun 311810151 <45851430+mukromun311810151@users.noreply.github.com>
2019-01-01 21:41:22 +0700

    Add files via upload

---
+++ menentukan bilangan ganjil atau genap.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int bilangan;
+	cout <<"masukkan bilangan : " <<endl;
+	cin >> bilangan;
+	
+	if (bilangan %2==0)
+	cout <<"Adalah bilangan genap" <<endl;
+	else
+	cout <<"Adalah bilangan ganjil" <<endl;
+	
+	return 0;
+}
